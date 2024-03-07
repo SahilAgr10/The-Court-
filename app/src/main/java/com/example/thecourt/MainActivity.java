f379d2610fec54c1c148192eada874c2ea163246
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-07 20:30:12 +0530

    the court Application

--- app/src/main/java/com/example/thecourt/MainActivity.java
+++ app/src/main/java/com/example/thecourt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.thecourt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThreeA(View view) {
+    }
+
+    public void addTwoA(View view) {
+    }
+
+    public void freeA(View view) {
+    }
+
+    public void addThreeB(View view) {
+    }
+
+    public void addTwoB(View view) {
+    }
+
+    public void freeB(View view) {
+    }
+
+    public void Reset(View view) {
     }
 }
